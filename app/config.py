aab5e358337001a5a1769be263e5d0a448b16d6f
LimeJus <87711575+Kakashi5255@users.noreply.github.com>
2025-09-03 23:06:51 +1000

    Clean config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
+# app/config.py
+# Application configuration settings.
+
 import os
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
 load_dotenv()
 
 APP_NAME = "ScamBot Backend"
